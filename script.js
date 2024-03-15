ef7270dfce0897046a6c06c35ddb68a022876795
harsha011 <143945670+hars0101@users.noreply.github.com>
2024-03-16 00:33:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeImage() {
+    document.getElementById("mainImage").style.display = "none";
+    document.getElementById("hoverImage").style.display = "block";
+}
+
+function restoreImage() {
+    document.getElementById("hoverImage").style.display = "none";
+    document.getElementById("mainImage").style.display = "block";
+}
